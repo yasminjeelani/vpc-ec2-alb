7ce9ce5e471f229ebd1a7ae9bc3334e346eba675
yasminjeelani <36299748+yasminjeelani@users.noreply.github.com>
2023-10-18 16:52:10 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -8,8 +8,7 @@ INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 # Install the AWS CLI
 apt install -y awscli
 
-# Download the images from S3 bucket
-#aws s3 cp s3://myterraformprojectbucket2023/project.webp /var/www/html/project.png --acl public-read
+
 
 # Create a simple HTML file with the portfolio content and display the images
 cat <<EOF > /var/www/html/index.html
